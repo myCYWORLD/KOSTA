dd41f4f93504391b962a49f6f360167d735a3454
yeon <choyeon960325@gmail.com>
2022-08-22 17:59:01 +0900

    MSA 실습

---
+++ MyMSA/Catalog2Hystrix/src/main/java/egovframework/msa/sample/Catalog2HystrixApplication.java
@@ -0,0 +1,13 @@
+package egovframework.msa.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Catalog2HystrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Catalog2HystrixApplication.class, args);
+	}
+
+}
---
+++ MyMSA/Catalogs2Hystrix/src/main/java/egovframework/msa/sample/Catalogs1Application.java
@@ -0,0 +1,20 @@
+package egovframework.msa.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+public class Catalogs1Application {
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(Catalogs1Application.class, args);
+	}
+
+}
